8dd11e4e8e4d506c9934324d2f38d1cbc05bea57
jordles113 <jordan.t.romero@gmail.com>
2021-02-24 15:42:43 -0600

    learning min and max values

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         
 
         int myValue = 10000; 
+
+        int myMinIntValue = Integer.MIN_VALUE; 
+        int myMaxIntValue = Integer.MAX_VALUE; 
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
     }
 }
