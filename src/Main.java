2fee6669744b80e274d72e7ddd09488d34c0cd4d
jordles113 <jordan.t.romero@gmail.com>
2021-02-24 16:10:30 -0600

    learning about bytes shorts and longs

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,28 @@ public class Main {
         int myMaxIntTest = 214_748_367; 
         // valid but not used oftern above 
 
+        byte myMinByteValue = Byte.MIN_VALUE; 
+        byte myMaxByteValue = Byte.MAX_VALUE; 
+
+        System.out.println("Byte Min Value = " + myMinByteValue); 
+        System.out.println("Byte Max Value = " + myMaxByteValue);
+
         short myMinShortValue = Short.MIN_VALUE; 
         short myMaxShortValue = Short.MAX_VALUE; 
 
         System.out.println("Short Min Value = " + myMinShortValue); 
         System.out.println("Short Max Value = " + myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE; 
+        long myMaxLongValue = Long.MAX_VALUE; 
+
+        System.out.println("Long Min Value = " + myMinLongValue); 
+        System.out.println("Long Max Value = " + myMaxLongValue);
+
+        long myLongValue = 100L; 
+
+        long bigLongLiteralValue = 214748367342L;
+        
+        short bigShortLiteralValue = 32767; 
     }
 }
